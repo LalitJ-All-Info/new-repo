53cd33461cca5abbc9abce05e743484bd59e6753
Lalit Jadaun <lalitthakurs9734@gmail.com>
2024-11-28 11:19:45 +0000

    change in script

--- backup_logs.sh
+++ backup_logs.sh
@@ -14,3 +14,22 @@ tar -czPf "$LOG_DIR/backup/log_$DATE.tar.gz" "$LOG_DIR"/*.log
 
 echo "Logs archived at: $LOG_DIR/backup/log_$DATE.tar.gz"
 
+
+
+#!/bin/bash
+
+# Set the log directory from the first argument or use the current directory
+LOG_DIR="${1:-.}"
+
+# Create the backup directory
+mkdir -p "$LOG_DIR/backup"
+
+# Get the current date and time
+DATE=$(date +'%Y-%m-%d_%H-%M-%S')
+
+# Archive and compress all .log files in the specified directory
+tar -czPf "$LOG_DIR/backup/log_$DATE.tar.gz" "$LOG_DIR"/*.log
+
+echo "Logs archived at: $LOG_DIR/backup/log_$DATE.tar.gz"
+
+
